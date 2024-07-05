aa01f0060519202983f221a1c04110841d37920d
VinTent26 <155293495+VinTent26@users.noreply.github.com>
2024-07-05 08:57:26 +0700

    danh mục sản phẩm

--- controllers/admin/product-category.contrller.js
+++ controllers/admin/product-category.contrller.js
@@ -1,21 +1,30 @@
 const ProductCategory = require("../../models/product-category.model")
 const systemConfig = require("../../config/system")
+const createTreeHelper = require("../../helpers/createTree")
 //[GET] /admin/products-category
 module.exports.index = async (req, res) => {
     let find = {
         deleted: false
     }
     const records = await ProductCategory.find(find)
+    const newRecords = createTreeHelper.tree(records)
 
     res.render("admin/pages/product-category/index", {
         pageTitle: "danh mục sản phẩm",
-        records: records,
+        records: newRecords,
     });
 }
 //[GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+
+    const records = await ProductCategory.find(find)
+    const newRecords = createTreeHelper.tree(records)
     res.render("admin/pages/product-category/create", {
         pageTitle: "Tạo danh mục sản phẩm",
+        records: newRecords
     });
 }
 //[POST] admin/products-category/create
