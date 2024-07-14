bc912361760780e1597ebbe44bab509dae0b7eb4
VinTent26 <155293495+VinTent26@users.noreply.github.com>
2024-07-14 08:25:30 +0700

    Phân quyền

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,43 @@
+const md5 = require('md5');
+const Account = require("../../models/account.model")
+const systemConfig = require("../../config/system");
+//[GET] admin/auth/login
+module.exports.login = (req, res) => {
+    if (req.cookies.token) {
+        res.redirect(`${systemConfig.prefixAdmin}/dashboard`)
+    } else {
+        res.render("admin/pages/auth/login", {
+            pageTitle: "trang đăng nhập"
+        });
+    }
+}
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+    const user = await Account.findOne({
+        email: email,
+        deleted: false
+    })
+    if (!user) {
+        req.flash("error", "Email không tồn tại")
+        res.redirect("back")
+        return
+    }
+    if (md5(password) != user.password) {
+        req.flash("error", "mật không đúng")
+        res.redirect("back")
+        return
+    }
+    if (user.status == "inactive") {
+        req.flash("error", "tài khoản đã bị khóa")
+        res.redirect("back")
+        return
+    }
+    res.cookie("token", user.token)
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`)
+}
+//[GET] admin/auth/login
+module.exports.logout = (req, res) => {
+    res.clearCookie("token")
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
+}
